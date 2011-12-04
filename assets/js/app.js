7c1911a1e245cfd2df411dc41d40f0cdc7027279
hendrik2009 <kontakt@hendrikkroeger.de>
2011-12-05 00:15:21 +0100

    bla

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,36 +3,57 @@
 //
 
 run(function () {
-	alert('app.js run');
+	
+	//alert('app.js run');
     // immediately invoked on first run
     var init = (function () {
         // init here
-        console.log('app.js run init');
-     
+        //alert('app.js run init');
+        function ask(){
+			/*x$('#welcome').xhr('inner', 'test.html');*/
+			if (window.XMLHttpRequest)
+			  {// code for IE7+, Firefox, Chrome, Opera, Safari
+			  xmlhttp=new XMLHttpRequest();
+			  }
+			else
+			  {// code for IE6, IE5
+			  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			  }
+			xmlhttp.open("GET","http://featherlike.restorm.com/widgets/minipage.xml",false);
+			xmlhttp.send();
+			xmlDoc=xmlhttp.responseXML;
+			
+			//document.write("<table border='1'>");
+			var x=xmlDoc.getElementsByTagName("genre");
+			for (i=0;i<x.length;i++)
+			  {
+			 // document.write("<tr><td>");
+			  //document.write(x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue);
+			 // document.write("</td><td>");
+			 	x$('#info').html('before', '<p>some warmup html</p>');
+
+			  }
+		};
     })();
 
-    // a little inline controller
+    // controller  
     when('#info', function(){
-    	alert('info init');
+    	//alert('info init');
     });
     when('#music', function(){
-    	alert('music init');
+    	//alert('music init');
     });
     when('#video', function(){
-    	alert('video init');
+    	//alert('video init');
     });
     when('#foto', function(){
-    	alert('foto init');
+    	//alert('foto init');
     });
     when('#event', function(){
-    	alert('event init');
+    	//alert('event init');
     });
-    when('#back', function(){
-    	alert('back init');
-    	display('#welcome');
-    });
-    
-   	alert('setup');
+
+   	//alert('setup');
    	x$(".back_button").on('touchstart', function () {
             display("#welcome");
     });
