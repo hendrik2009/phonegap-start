f3e00ed536e3fe7189c617260ea4921b6b438c39
hendrik2009 <kontakt@hendrikkroeger.de>
2011-12-05 00:58:34 +0100

    bla

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,36 +7,41 @@ run(function () {
 	//alert('app.js run');
     // immediately invoked on first run
     var init = (function () {
-        // init here
-        //alert('app.js run init');
-        
-        function ask(){
-			/*x$('#welcome').xhr('inner', 'test.html');*/
-			if (window.XMLHttpRequest)
-			  {// code for IE7+, Firefox, Chrome, Opera, Safari
-			  xmlhttp=new XMLHttpRequest();
-			  }
-			else
-			  {// code for IE6, IE5
-			  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-			  }
+
+		function ask(){			
+			if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp			= new XMLHttpRequest();
+			}
+			else{// code for IE6, IE5
+				xmlhttp			= new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			
+			//loading XML
 			xmlhttp.open("GET","http://featherlike.restorm.com/widgets/minipage.xml",false);
 			xmlhttp.send();
-			xmlDoc=xmlhttp.responseXML;
+			xmlDoc				= xmlhttp.responseXML;
 			
-			//document.write("<table border='1'>");
-			var x=xmlDoc.getElementsByTagName("genre");
-			for (i=0;i<x.length;i++)
-			  {
-			 // document.write("<tr><td>");
-			  //document.write(x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-			 // document.write("</td><td>");
-			 	x$("#info").html('before', '<p>some warmup html</p>');
-
-			  }
-		};
+			var x				= xmlDoc.getElementsByTagName("tab");
+			var message;
+			message				= "<p>:::Here we go:::</p>"
+			
+			for (i=0;i<x.length;i++){
+				
+			 	for (var u = 0; u < x[i].attributes.length; u++) {
+			 		
+					var attr = info.attributes[u];
+					if (attr.specified) {
+						message += "<p>" + attr.name + " = "  + attr.value + "</p>";
+            		}
+            	
+            	}
+			}
+			x$("#info").html('inner',"msg:"+message);
+		
+		};// end ask
 		ask();
-    })();
+		
+	})();//end init
 
     // controller  
     when('#info', function(){
@@ -59,6 +64,4 @@ run(function () {
    	x$(".back_button").on('touchstart', function () {
             display("#welcome");
     });
-    
 });
-
